a2f3c58162c639cc1ff0a1487ca2dc3f0cf73177
hhhuuunnnggg-design <nguyendinhhungtc2020@gmail.com>
2026-05-04 21:56:52 +0700

    up admin

--- admin-app/src/main/java/com/tuyensinh/admin/ui/panels/DiemDGNLVSATImportPanel.java
+++ admin-app/src/main/java/com/tuyensinh/admin/ui/panels/DiemDGNLVSATImportPanel.java
@@ -171,7 +171,9 @@ public class DiemDGNLVSATImportPanel extends JPanel {
         SwingWorker<ImportSummary, String> worker = new SwingWorker<>() {
             @Override
             protected ImportSummary doInBackground() {
-                return importExcel(selectedFile, this::publish);
+                //  return importExcel(selectedFile, this::publish);
+                // return null;
+                return importExcel(selectedFile, msg -> publish(msg));
             }
 
             @Override
